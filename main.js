d0bdd6f9d3fb1cc5ae9bfb8234163001ba41b5b0
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-10-24 22:14:06 -0400

    added more css

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-let myArray = ["Head", "Tales"];
+let myArray = ["Head", "Tail"];
 
-document.querySelector("button").addEventListener("click", coinToss);
+// document.querySelector("button").addEventListener("click", coinToss);
+document.querySelector(".head").addEventListener("click", coinToss);
+document.querySelector(".tails").addEventListener("click", coinTossTwo);
 
+// function coinToss() {
+//   let botCoin = myArray[Math.floor(Math.random() * myArray.length)];
 
+//   document.querySelector("h2").innerText = botCoin;
+// }
 
 function coinToss() {
-    let botCoin = myArray[Math.floor(Math.random() * myArray.length)];
+  let botCoin = myArray[Math.floor(Math.random() * myArray.length)];
+  document.querySelector("h2").innerText = botCoin;
+  checkWhoWon(botCoin, "head");
+}
 
-    document.querySelector('h2').innerText = botCoin
+function coinTossTwo() {
+  let botCoin = myArray[Math.floor(Math.random() * myArray.length)];
+  document.querySelector("h2").innerText = botCoin;
+  checkWhoWon(botCoin, "tail");
+}
+
+function checkWhoWon(botCoin, playersCoin) {
+  if (botCoin == "Head" && playersCoin == "head") {
+    document.querySelector("h4").innerText = "You guessed corretly";
+  } else if (botCoin == "Head" && playersCoin == "tail") {
+    document.querySelector("h4").innerText = "Try Again Loser";
+  } else if (botCoin == "Tail" && playersCoin == "tail") {
+    document.querySelector("h4").innerText = "You guessed corretly";
+  } else if (botCoin == "Tail" && playersCoin == "head") {
+    document.querySelector("h4").innerText = "Try Again Loser";
+  }
 }
